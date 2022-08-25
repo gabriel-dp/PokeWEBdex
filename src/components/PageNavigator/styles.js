1606932ab919af9f4785828e954bbb413764db4a
gabrieldp23 <gabriel.meira.2004@gmail.com>
2022-08-24 22:54:16 -0300

    🎨 pagination button style change when disabled

--- src/components/PageNavigator/styles.js
+++ src/components/PageNavigator/styles.js
@@ -15,14 +15,15 @@ export const ButtonsContainer = styled.div`
 export const NavButton = styled.button`
     width: 50%;
     height: 100%;
+    background-color: #c5c5c5;
     border-radius: 0;
     border: none;
     user-select: none;
     -webkit-tap-highlight-color: transparent;
 
-    background-color: ${props => props.isDisabled ? 'transparent' : '#c5c5c5'};
-    cursor: ${props => !props.isDisabled ? 'pointer' : 'normal'};
-    color: ${props => !props.isDisabled ? '#333' : 'transparent'};
+    opacity: ${props => props.isDisabled ? 0.4 : 1};
+    cursor: ${props => !props.isDisabled ? 'pointer' : 'not-allowed'};
+    color: ${props => !props.isDisabled ? '#333333' : '#33333377'};
 
     @media (min-width: 688px) {
         ${props => !props.isDisabled ? ':hover { background: linear-gradient(135deg, #cc1100, #8b0000); color: #fff; }' : ''};
