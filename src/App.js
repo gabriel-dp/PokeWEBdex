73a20e450d67df604edceba75e3d168defc61362
gabrieldp23 <gabriel.meira.2004@gmail.com>
2022-08-27 09:15:20 -0300

    ⚙️ switch to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
 import PokeData from './pages/PokeData';
@@ -6,11 +6,11 @@ import PokeDex from './pages/PokeDex';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <GlobalStyle/>
       <Route path='/' exact component={PokeDex}/>
       <Route path='/pokemon/:nameORid' component={PokeData}/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
